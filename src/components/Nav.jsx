0f3dcb8feb5562bba3f6a8db08e9b7fbcc2f8dc2
paskkal.io <pascalobiora73@gmail.com>
2024-05-25 00:20:03 +0100

    completed landin page v1

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,48 +1,65 @@
+import { useState } from "react";
 import { Link } from "react-router-dom";
 import schoolLogo from "../assets/images/dghcslogo.png";
 import styles from "./Nav.module.css";
+
+const NavLinks = [
+  {
+    location: "/",
+    title: "home",
+  },
+  {
+    location: "about",
+    title: "about",
+  },
+  {
+    location: "programmes",
+    title: "programmes",
+  },
+  {
+    location: "blog",
+    title: "blog",
+  },
+  {
+    location: "contact",
+    title: "contact",
+  },
+  {
+    location: "login",
+    title: "portal",
+  },
+];
 function Nav() {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+  function handleOpenMenu() {
+    setIsMenuOpen((isMenuOpen) => !isMenuOpen);
+  }
   return (
     <nav className={styles.nav}>
       <Link to="/">
         <img src={schoolLogo} className={styles.nav_image} />
       </Link>
-      <div className={styles.nav_links} id="navLinks">
-        <i className="fa fa-times"></i>
-        <ul>
-          <li>
-            <Link to="/">
-              <strong>HOME</strong>
-            </Link>
-          </li>
-          <li>
-            <Link to="about">
-              <strong>ABOUT</strong>
-            </Link>
-          </li>
-          <li>
-            <Link to="programmes">
-              <strong>PROGRAMMES</strong>
-            </Link>
-          </li>
-          <li>
-            <Link to="blog">
-              <strong>BLOG</strong>
-            </Link>
-          </li>
-          <li>
-            <Link to="contact">
-              <strong>CONTACT</strong>
-            </Link>
-          </li>
-          <li>
-            <Link to="login">
-              <strong>PORTAL</strong>
-            </Link>
-          </li>
+      <div className={styles.toggle} onClick={handleOpenMenu}>
+        {isMenuOpen ? (
+          <i className="fa fa-times"></i>
+        ) : (
+          <i className="fa fa-bars"></i>
+        )}
+      </div>
+      <div
+        className={`${styles.nav_links} ${isMenuOpen ? styles.menu_active : ""}`}
+        id="navLinks"
+      >
+        <ul className={styles.nav_link_list}>
+          {NavLinks.map((ele) => (
+            <li key={ele} onClick={handleOpenMenu}>
+              <Link to={ele.location}>
+                <strong className="uppercase">{ele.title}</strong>
+              </Link>
+            </li>
+          ))}
         </ul>
       </div>
-      <i className="fa fa-bars"></i>
     </nav>
   );
 }
